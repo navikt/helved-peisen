4db8d60826521b25238570f7a648353fe8e3d872
Stephen Ramthun <stephenramthun@gmail.com>
2025-03-27 13:48:08 +0100

    Memoiser data som skal inn i diagrammet

--- app/kafka/timeline/MessagesChart.tsx
+++ app/kafka/timeline/MessagesChart.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useLayoutEffect, useMemo, useState } from 'react'
 import clsx from 'clsx'
-import { v4 } from 'uuid'
 import { Bar } from 'react-chartjs-2'
 import {
     BarController,
@@ -12,45 +11,39 @@ import {
     ChartOptions,
     LinearScale,
 } from 'chart.js'
+import { format } from 'date-fns/format'
 
 import type { Message } from '@/app/kafka/timeline/types'
 import { useMessageMap } from '@/app/kafka/timeline/useMessageMap.ts'
 
 import styles from './MessagesChart.module.css'
-import { format } from 'date-fns/format'
 
 Chart.register(BarElement, BarController, CategoryScale, LinearScale)
-
-const getCSSPropertyValue = (property: string) => {
-    return getComputedStyle(
-        document.body.getElementsByTagName('main')[0]
-    ).getPropertyValue(property)
-}
 
 const messages: Record<string, Message[]> = {
     'helved.oppdrag.v1': [
         {
-            key: v4(),
+            key: '1',
             timestamp: '2025-03-25T23:00:00.000Z',
             data: '',
         },
         {
-            key: v4(),
+            key: '2',
             timestamp: '2025-03-25T22:00:00.000Z',
             data: '',
         },
         {
-            key: v4(),
+            key: '3',
             timestamp: '2025-03-25T21:00:00.000Z',
             data: '',
         },
         {
-            key: v4(),
+            key: '4',
             timestamp: '2025-03-24T23:00:00.000Z',
             data: '',
         },
         {
-            key: v4(),
+            key: '5',
             timestamp: '2025-03-23T23:00:00.000Z',
             data: '',
         },
@@ -59,12 +52,12 @@ const messages: Record<string, Message[]> = {
     'helved.dryrun-aap.v1': [],
     'helved.dryrun-ts.v1': [
         {
-            key: v4(),
+            key: '6',
             timestamp: '2025-03-21T23:00:00.000Z',
             data: '',
         },
         {
-            key: v4(),
+            key: '7',
             timestamp: '2025-03-20T23:00:00.000Z',
             data: '',
         },
@@ -73,6 +66,12 @@ const messages: Record<string, Message[]> = {
     'helved.kvittering.v1': [],
     'helved.status.v1': [],
     'helved.kvittering-queue.v1': [],
+}
+
+const getCSSPropertyValue = (property: string) => {
+    return getComputedStyle(
+        document.body.getElementsByTagName('main')[0]
+    ).getPropertyValue(property)
 }
 
 type Props = React.HTMLAttributes<HTMLDivElement>
@@ -86,41 +85,54 @@ export const MessagesChart: React.FC<Props> = ({ className, ...rest }) => {
         barHoverColor: '',
     })
 
-    const data = {
-        labels: Object.keys(messageMap).map(it => format(it, "yyyy-MM-dd, HH:mm")),
-        datasets: [
-            {
-                data: Object.values(messageMap).map((it) => it.length),
-                barPercentage: 1,
-                categoryPercentage: 1,
-                backgroundColor: colors.barColor,
-                hoverBackgroundColor: colors.barHoverColor,
-            },
-        ],
-    }
+    const data = useMemo(
+        () => ({
+            labels: Object.keys(messageMap).map((it) =>
+                format(it, 'yyyy-MM-dd, HH:mm')
+            ),
+            datasets: [
+                {
+                    data: Object.values(messageMap).map((it) => it.length),
+                    barPercentage: 1,
+                    categoryPercentage: 1,
+                    backgroundColor: colors.barColor,
+                    hoverBackgroundColor: colors.barHoverColor,
+                },
+            ],
+        }),
+        [messageMap, colors]
+    )
 
-    const options: ChartOptions<'bar'> = {
-        maintainAspectRatio: false,
-        scales: {
-            x: {
-                ticks: {
-                    color: colors.labelColor,
+    const options: ChartOptions<'bar'> = useMemo(
+        () => ({
+            animation: {
+                duration: 0,
+            },
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    ticks: {
+                        color: colors.labelColor,
+                    },
+                },
+                y: {
+                    ticks: {
+                        color: colors.labelColor,
+                    },
                 },
             },
-            y: {
-                ticks: {
-                    color: colors.labelColor,
-                },
-            },
-        },
-    }
+        }),
+        [colors]
+    )
 
     useLayoutEffect(() => {
         const updateColor = () => {
             setColors({
-                labelColor: getCSSPropertyValue('--ax-text-neutral-subtle'),
+                labelColor: getCSSPropertyValue('--ax-text-neutral'),
                 barColor: getCSSPropertyValue('--ax-bg-accent-strong'),
-                barHoverColor: getCSSPropertyValue('--ax-bg-accent-strong-hover'),
+                barHoverColor: getCSSPropertyValue(
+                    '--ax-bg-accent-strong-hover'
+                ),
             })
         }
 
