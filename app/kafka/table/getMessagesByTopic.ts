8afc56e08345502096faa9bd357bde0a74ebcd47
Stephen Ramthun <stephenramthun@gmail.com>
2025-03-28 13:44:39 +0100

    Filtrer på topic

--- app/kafka/table/getMessagesByTopic.ts
+++ app/kafka/table/getMessagesByTopic.ts
@@ -5,7 +5,18 @@ import { TestData } from '@/fakes/testData.ts'
 export const getMessagesByTopic = async (params: SearchParams) => {
     const fom = params.fom ? new Date(params.fom) : null
     const tom = params.tom ? new Date(params.tom) : null
-    return fom && tom
-        ? TestData.messagesByTopic({ fom, tom })
-        : TestData.messagesByTopic()
+    const topics = params.topics?.split(',')
+
+    const filteredByDates =
+        fom && tom
+            ? TestData.messagesByTopic({ fom, tom })
+            : TestData.messagesByTopic()
+
+    const filteredByTopics = topics
+        ? Object.entries(filteredByDates).filter(([topic, _]) =>
+              topics.includes(topic)
+          )
+        : Object.entries(filteredByDates)
+
+    return Object.fromEntries(filteredByTopics)
 }
