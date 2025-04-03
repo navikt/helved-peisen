7163d618d72c5a32e57143ddb63bc4bac2b5a8ec
JorgenNyborgChristensen <jorgen.nyborg-christensen@nav.no>
2025-04-03 15:57:15 +0200

    Det er noe typescript krøll her
    SyntaxError: The requested module '@playwright/test' does not provide an export named 'Page'

--- tests/filters.helpers.ts
+++ tests/filters.helpers.ts
@@ -1,22 +1,22 @@
-import { expect, Page } from '@playwright/test'
+import { expect } from '@playwright/test'
 import { deslugify } from '@/lib/string.ts'
 
-const clickCombobox = async (text: string, page: Page) => {
+const clickCombobox = async (text: string, page) => {
     const button = page.getByText(text).first()
     await button.click()
 }
 
-const clickOption = async (text: string, page: Page) => {
+const clickOption = async (text: string, page) => {
     const option = page.getByRole('option').filter({ hasText: text })
     await option.isVisible()
     await option.click()
 }
 
-export const waitForTasksToLoad = async (page: Page) => {
+export const waitForTasksToLoad = async (page) => {
     await page.getByText('Rekjør').first().waitFor({ state: 'visible' })
 }
 
-export const selectStatus = async (status: TaskStatus, page: Page) => {
+export const selectStatus = async (status: TaskStatus, page) => {
     await clickCombobox('Status', page)
     await clickOption(deslugify(status), page)
 
@@ -25,23 +25,23 @@ export const selectStatus = async (status: TaskStatus, page: Page) => {
     await page.keyboard.press('Escape')
 }
 
-export const expectStatuses = (expected: TaskStatus[], page: Page) => {
+export const expectStatuses = (expected: TaskStatus[], page) => {
     let url = new URL(page.url())
     expect(url.searchParams.get('status')).toEqual(expected.join(','))
 }
 
-export const deselectStatus = async (status: TaskStatus, page: Page) => {
+export const deselectStatus = async (status: TaskStatus, page) => {
     const button = page.getByRole('button', { name: status })
     await button.click()
     await expect(button).toHaveCount(0)
 }
 
-export const selectType = async (type: TaskKind, page: Page) => {
+export const selectType = async (type: TaskKind, page) => {
     await clickCombobox('Type', page)
     await clickOption('Iverksetting', page)
 }
 
-export const expectType = (type: TaskKind, page: Page) => {
+export const expectType = (type: TaskKind, page) => {
     let url = new URL(page.url())
     expect(url.searchParams.get('kind')).toEqual(type)
 }
