58d182abd967f71ddd313be892246405c0ac96f0
Stephen Ramthun <stephenramthun@gmail.com>
2025-04-22 13:16:48 +0200

    Bytt default route

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/scheduler',
+                destination: '/kafka',
                 permanent: false,
             },
         ]
