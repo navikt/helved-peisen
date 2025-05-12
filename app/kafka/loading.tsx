95c483b354cf1c68412bb0a84ac4cb74c4f89a5b
JorgenNyborgChristensen <jorgen.nyborg-christensen@nav.no>
2025-05-12 11:45:22 +0200

    Prøv å fjern filter fra loading

--- app/kafka/loading.tsx
+++ app/kafka/loading.tsx
@@ -1,4 +1,3 @@
-import { Filtere } from '@/app/kafka/Filtere.tsx'
 import { MessagesTableSkeleton } from '@/app/kafka/table/MessagesTable.tsx'
 import { MessagesChartSkeleton } from '@/app/kafka/chart/MessagesChart.tsx'
 
@@ -7,7 +6,6 @@ import styles from '@/app/kafka/page.module.css'
 export default function Loading() {
     return (
         <section className={styles.page}>
-            <Filtere className={styles.filtere} />
             <MessagesChartSkeleton />
             <MessagesTableSkeleton />
         </section>
