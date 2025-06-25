8251d1c5e681e9addc61516ae1bd5df8698708e9
Stephen Ramthun <stephenramthun@gmail.com>
2025-06-25 13:23:27 +0200

    Ikke optimiser bilder

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
         optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
         authInterrupts: true,
     },
+    images: {
+        unoptimized: true,
+    },
     async redirects() {
         return [
             {
