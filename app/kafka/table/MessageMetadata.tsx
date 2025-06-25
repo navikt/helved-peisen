7493e26a42fbb08b9b022d7b22c2cddebf1392c6
Stephen Ramthun <stephenramthun@gmail.com>
2025-06-25 12:58:09 +0200

    Kan ikke bruke DOMParser i backend

--- app/kafka/table/MessageMetadata.tsx
+++ app/kafka/table/MessageMetadata.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { ReactNode } from 'react'
 import { BodyShort, BoxNew, HStack, Label, VStack } from '@navikt/ds-react'
 
--- app/kafka/table/TopicNameTag.tsx
+++ app/kafka/table/TopicNameTag.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { Tag, TagProps } from '@navikt/ds-react'
 
