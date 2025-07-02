8e79d1a505407e8f62bd6d163eab277da9cfde68
Stephen Ramthun <stephenramthun@gmail.com>
2025-07-02 09:28:27 +0200

    Ikke reset skjema om validering feiler

--- app/sak/Filtere.tsx
+++ app/sak/Filtere.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useActionState } from 'react'
 import { Select, TextField } from '@navikt/ds-react'
 
 import { showToast } from '@/components/Toast'
@@ -10,14 +10,42 @@ import { fetchSak } from './actions'
 
 import styles from './Filtere.module.css'
 
+type FormState = {
+    sakId?: { value?: string; error?: string }
+    fagsystem?: { value?: string; error?: string }
+}
+
+const defaultState: FormState = {
+    sakId: undefined,
+    fagsystem: undefined,
+}
+
 type Props = React.HTMLAttributes<HTMLDivElement>
 
 export const Filtere: React.FC<Props> = ({ className, ...rest }) => {
     const { setSak } = useSak()
 
-    const getSak = async (formData: FormData) => {
-        const sakId = formData.get('sakId') as string
-        const fagsystem = formData.get('fagsystem') as string
+    const getSak = async (_: FormState, formData: FormData) => {
+        const { sakId, fagsystem } = Object.fromEntries(formData)
+
+        if (
+            typeof sakId !== 'string' ||
+            sakId.length === 0 ||
+            typeof fagsystem !== 'string' ||
+            fagsystem.length === 0
+        ) {
+            return {
+                sakId: {
+                    value: sakId as string | undefined,
+                    error: !sakId ? 'Sak-ID må fylles ut' : undefined,
+                },
+                fagsystem: {
+                    value: fagsystem as string | undefined,
+                    error: !fagsystem ? 'Fagsystem må fylles ut' : undefined,
+                },
+            }
+        }
+
         const response = await fetchSak(sakId, fagsystem)
 
         if (response.data) {
@@ -35,24 +63,30 @@ export const Filtere: React.FC<Props> = ({ className, ...rest }) => {
         if (response.error) {
             showToast(response.error.message, { variant: 'error' })
         }
+
+        return defaultState
     }
+
+    const [state, action] = useActionState(getSak, defaultState)
 
     return (
         <div {...rest}>
-            <form className={styles.form} action={getSak}>
+            <form className={styles.form} action={action}>
                 <TextField
                     className={styles.input}
                     label="Sak-ID"
                     name="sakId"
                     size="small"
-                    required
+                    defaultValue={state.sakId?.value}
+                    error={state.sakId?.error}
                 />
                 <Select
                     className={styles.input}
                     label="Fagsystem"
                     name="fagsystem"
                     size="small"
-                    required
+                    defaultValue={state.fagsystem?.value}
+                    error={state.fagsystem?.error}
                 >
                     <option value="">- Velg fagsystem -</option>
                     <option value="DP">Dagpenger</option>
