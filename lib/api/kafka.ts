66c8a51bae8ea754913abc4f732399e86bae57fe
Stephen Ramthun <stephenramthun@gmail.com>
2025-09-01 09:40:48 +0200

    Sett timeout på henting av kafka-meldinger til 20 sekunder

--- lib/api/kafka.ts
+++ lib/api/kafka.ts
@@ -10,30 +10,45 @@ export const fetchMessages = async (
     searchParams: URLSearchParams
 ): Promise<ApiResponse<Message[]>> => {
     const apiToken = await getApiTokenFromCookie()
-    const response = await fetch(
-        `${Routes.external.kafka}?${searchParams.toString()}`,
-        {
-            headers: {
-                Authorization: `Bearer ${apiToken}`,
-            },
-        }
-    )
+    const signal = AbortSignal.timeout(20_000)
 
-    if (response.ok) {
-        return {
-            data: await response.json(),
-            error: null,
-        }
-    } else {
-        logger.error(
-            `Klarte ikke hente Kafka data: ${response.status} - ${response.statusText}`
-        )
-        return {
-            data: null,
-            error: {
-                message: `Klarte ikke hente Kafka data: ${response.status} - ${response.statusText}`,
-                statusCode: response.status,
-            },
-        }
-    }
+    return fetch(`${Routes.external.kafka}?${searchParams.toString()}`, {
+        headers: {
+            Authorization: `Bearer ${apiToken}`,
+        },
+        signal,
+    })
+        .then(async (response) => {
+            if (response.ok) {
+                return {
+                    data: await response.json(),
+                    error: null,
+                }
+            } else {
+                throw response
+            }
+        })
+        .catch((error) => {
+            if (error.name === 'TimeoutError') {
+                logger.error('Timeout ved henting av Kafka data')
+                return {
+                    data: null,
+                    error: {
+                        message: 'Timeout ved henting av Kafka data',
+                        statusCode: 504,
+                    },
+                }
+            } else {
+                logger.error(
+                    `Klarte ikke hente Kafka data: ${error.status} - ${error.statusText}`
+                )
+                return {
+                    data: null,
+                    error: {
+                        message: `Klarte ikke hente Kafka data: ${error.status} - ${error.statusText}`,
+                        statusCode: error.status,
+                    },
+                }
+            }
+        })
 }
