da036513c864a235ba9414532e9dd47ee2907877
Stephen Ramthun <stephenramthun@gmail.com>
2025-09-04 09:42:23 +0200

    Test filtrering gamle meldinger

--- app/kafka/table/MessagesTable.test.tsx
+++ app/kafka/table/MessagesTable.test.tsx
@@ -4,6 +4,7 @@ import { MessagesTable } from './MessagesTable'
 import type { Message } from '@/app/kafka/types.ts'
 import { TestData } from '@/fakes/testData'
 import { userEvent } from '@testing-library/user-event'
+import { beforeEach } from 'node:test'
 
 describe('MessagesTable', () => {
     test('sorterer på timestamp', async () => {
@@ -39,5 +40,27 @@ describe('MessagesTable', () => {
         expect(within(rows[1]).getByText('123')).toBeVisible()
         expect(within(rows[2]).getByText('369')).toBeVisible()
         expect(within(rows[3]).getByText('246')).toBeVisible()
+    })
+
+    test('filtrerer vekk alle utenom de siste meldingene på samme nøkkel og topic', async () => {
+        const user = userEvent.setup()
+
+        const messages: Record<string, Message[]> = {
+            'helved.oppdrag.v1': [
+                TestData.message({ timestamp_ms: 0, key: 'a' }),
+                TestData.message({ timestamp_ms: 1, key: 'a' }),
+                TestData.message({ timestamp_ms: 0, key: 'b' }),
+            ],
+            'helved.kvittering.v1': [
+                TestData.message({ topic_name: 'helved.kvittering.v1', timestamp_ms: 2, key: 'a' }),
+                TestData.message({ topic_name: 'helved.kvittering.v1', timestamp_ms: 0, key: 'b' }),
+                TestData.message({ topic_name: 'helved.kvittering.v1', timestamp_ms: 1, key: 'b' }),
+            ],
+        }
+        render(<MessagesTable messages={messages} />)
+        expect(screen.getAllByRole('row')).toHaveLength(7)
+
+        await user.click(screen.getByText('Siste'))
+        expect(screen.getAllByRole('row')).toHaveLength(5)
     })
 })
