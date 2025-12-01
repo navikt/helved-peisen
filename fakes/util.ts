f14c99c2dce4f8563fe72725c31751cb2d99eebc
Stephen Ramthun <stephenramthun@gmail.com>
2025-12-01 10:36:11 +0100

    Bytt til pnpm

--- fakes/util.ts
+++ fakes/util.ts
@@ -1,11 +1,9 @@
-import { type ParsedQs } from 'qs'
-
 export const sleep = (ms: number) =>
     new Promise((resolve) => {
         setTimeout(resolve, ms)
     })
 
-export const parseStringQueryParam = (value?: string | ParsedQs | (string | ParsedQs)[]): string[] | undefined =>
+export const parseStringQueryParam = (value?: any): string[] | undefined =>
     typeof value === 'string'
         ? value.split(',')
         : Array.isArray(value) && value.every((it) => typeof it === 'string')
