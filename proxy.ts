c67dbf34b1f33ed2b5da061e01d3ce80445fadca
Stephen Ramthun <stephenramthun@gmail.com>
2026-02-12 20:58:29 +0100

    Proxy støtter ikke NextJS server function som f.eks. redirect() eller unauthorized()

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,8 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
-import { isFaking } from '@/lib/env'
-import { fetchApiToken, fetchUtsjekkApiToken } from '@/lib/server/auth.ts'
-import { expiresIn } from '@navikt/oasis'
+import { type NextRequest, NextResponse } from 'next/server'
+import { expiresIn, requestAzureClientCredentialsToken } from '@navikt/oasis'
+import { logger } from '@navikt/next-logger'
+
+import { isFaking, requireEnv } from '@/lib/env'
 
 const isLocal = process.env.NODE_ENV !== 'production'
 
@@ -27,10 +27,20 @@ function handleLocalApiToken() {
     return response
 }
 
+async function fetchToken(scope: string) {
+    const result = await requestAzureClientCredentialsToken(scope)
+    if (!result.ok) {
+        logger.error(`Henting av token feilet: ${result.error.message}`)
+        throw Error(`Henting av token feilet: ${result.error.message}`)
+    }
+
+    return result.token
+}
+
 async function handleApiTokens() {
     const response = NextResponse.next()
 
-    const apiToken = await fetchApiToken()
+    const apiToken = await fetchToken(requireEnv('API_SCOPE'))
     response.cookies.set({
         name: 'api-token',
         value: apiToken,
@@ -40,7 +50,7 @@ async function handleApiTokens() {
         path: '/',
     })
 
-    const utsjekkToken = await fetchUtsjekkApiToken()
+    const utsjekkToken = await fetchToken(requireEnv('UTSJEKK_API_SCOPE'))
     response.cookies.set({
         name: 'utsjekk-api-token',
         value: utsjekkToken,
@@ -54,7 +64,7 @@ async function handleApiTokens() {
 }
 
 // noinspection JSUnusedGlobalSymbols
-export async function proxy(request: NextRequest): Promise<NextResponse> {
+export async function proxy(req: NextRequest): Promise<NextResponse> {
     if (isFaking) {
         return NextResponse.next()
     }
@@ -63,8 +73,8 @@ export async function proxy(request: NextRequest): Promise<NextResponse> {
         return handleLocalApiToken()
     }
 
-    const apiToken = request.headers.get('api-token')
-    const utsjekkToken = request.headers.get('utsjekk-api-token')
+    const apiToken = req.headers.get('api-token')
+    const utsjekkToken = req.headers.get('utsjekk-api-token')
 
     if (!apiToken || isExpired(apiToken) || !utsjekkToken || isExpired(utsjekkToken)) {
         return handleApiTokens()
