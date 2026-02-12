c0f0e453d39d062acb052d777793d8e73260d348
Stephen Ramthun <stephenramthun@gmail.com>
2026-02-12 10:49:04 +0100

    Typo

--- app/kafka/RefreshButton.tsx
+++ app/kafka/RefreshButton.tsx
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import { Button, Loader } from '@navikt/ds-react'
 import { ArrowsCirclepathIcon } from '@navikt/aksel-icons'
 import { MessagesContext } from './context/MessagesContext.tsx'
 
 export const RefreshButton = () => {
-    const { loading, fetchAdditionalMessages } = useContext(MessagesContext)
+    const { loading, fetchMessages } = useContext(MessagesContext)
     return (
-        <Button size="small" variant={loading ? 'secondary' : 'secondary-neutral'} onClick={fetchAdditionalMessages}>
+        <Button size="small" variant={loading ? 'secondary' : 'secondary-neutral'} onClick={fetchMessages}>
             {loading ? <Loader size="xsmall" /> : <ArrowsCirclepathIcon />}
         </Button>
     )
