c1190188571c08076c202da99ddbb8e0ccfd50e9
Stephen Ramthun <stephenramthun@gmail.com>
2026-03-03 10:30:57 +0100

    Sjekk at vi er pålogget i avstemmings-tabben

--- app/avstemming/page.tsx
+++ app/avstemming/page.tsx
@@ -3,8 +3,10 @@ import { LatestAvstemminger } from '@/app/avstemming/LatestAvstemminger'
 import { AvstemmingDryrun } from '@/app/avstemming/AvstemmingDryrun.tsx'
 import { fetchAvstemminger } from '@/lib/io.ts'
 import { AvstemmingDryrunV2 } from '@/app/avstemming/AvstemmingDryrunV2.tsx'
+import { checkToken } from '@/lib/server/auth'
 
 export default async function AvstemmingPage() {
+    await checkToken()
     const xmlMessages = await fetchAvstemminger()
 
     return (
