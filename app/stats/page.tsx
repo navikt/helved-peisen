5f51017b7558aed888bd9695e92d73579972eaed
JorgenNyb <201098578+JorgenNyb@users.noreply.github.com>
2026-03-06 14:46:25 +0100

    Darkmode på metbase embed

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -6,7 +6,7 @@ export default async function StatsView() {
     return (
         <section className="flex-1 h-full max-h-full relative flex flex-col p-4">
             <iframe
-                src="https://metabase.ansatt.nav.no/public/dashboard/be77dd93-0740-45e1-870e-c9b602505408"
+                src="https://metabase.ansatt.nav.no/public/dashboard/be77dd93-0740-45e1-870e-c9b602505408#theme=night"
                 width="100%"
                 height="800"
                 style={{ border: "none" }}
