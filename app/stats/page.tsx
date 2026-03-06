d20e44271a6255100a6f4fa2db24f4912efbf0e6
JorgenNyb <201098578+JorgenNyb@users.noreply.github.com>
2026-03-06 14:43:10 +0100

    Testing testing embed metabase dashboard i peisen for stats

---
+++ app/stats/page.tsx
@@ -0,0 +1,16 @@
+import { checkToken } from '@/lib/server/auth.ts'
+
+export default async function StatsView() {
+    await checkToken()
+
+    return (
+        <section className="flex-1 h-full max-h-full relative flex flex-col p-4">
+            <iframe
+                src="https://metabase.ansatt.nav.no/public/dashboard/be77dd93-0740-45e1-870e-c9b602505408"
+                width="100%"
+                height="800"
+                style={{ border: "none" }}
+            />
+        </section>
+    )
+}
