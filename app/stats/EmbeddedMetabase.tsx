b5c5f66ede1e297ec564a0799e0767d14406cf02
Stephen Ramthun <stephenramthun@gmail.com>
2026-03-16 11:26:08 +0100

    Ikke vis iframe for metabase før innhold er lastet for å slippe hvit flash

--- app/stats/EmbeddedMetabase.tsx
+++ app/stats/EmbeddedMetabase.tsx
@@ -1,14 +1,27 @@
 'use client'
 
 import { useTheme } from '@/hooks/useTheme.tsx'
+import { Loader } from '@navikt/ds-react'
+import clsx from 'clsx'
+import { useState } from 'react'
 
 export const EmbeddedMetabase: React.FC = () => {
+    const [show, setShow] = useState(false)
     const theme = useTheme() ?? 'light'
+
     return (
-        <iframe
-            src={`https://metabase.ansatt.nav.no/public/dashboard/be77dd93-0740-45e1-870e-c9b602505408#theme=${theme}&bordered=false&titled=false`}
-            className="w-full h-full bg-(--ax-bg-default)"
-            style={{ border: 'none' }}
-        />
+        <>
+            <iframe
+                src={`https://metabase.ansatt.nav.no/public/dashboard/be77dd93-0740-45e1-870e-c9b602505408#theme=${theme}&bordered=false&titled=false`}
+                className={clsx('w-full h-full bg-(--ax-bg-default)', show ? 'visible' : 'hidden')}
+                style={{ border: 'none' }}
+                onLoad={() => setShow(true)}
+            />
+            {!show && (
+                <div className="w-full flex justify-center">
+                    <Loader />
+                </div>
+            )}
+        </>
     )
 }
