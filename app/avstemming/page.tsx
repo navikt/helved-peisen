39d8bed2a9117c8309d259f22f2ea8403d90047f
JorgenNyb <201098578+JorgenNyb@users.noreply.github.com>
2026-05-05 16:13:10 +0200

    #498 Revert denne commiten etter å ha verifisert at audit log funker

--- app/avstemming/page.tsx
+++ app/avstemming/page.tsx
@@ -2,6 +2,7 @@ import { AvstemmingTimeline } from '@/app/avstemming/AvstemmingTimeline.tsx'
 import { LatestAvstemminger } from '@/app/avstemming/LatestAvstemminger'
 import { AvstemmingDryrun } from '@/app/avstemming/AvstemmingDryrun.tsx'
 import { AvstemmingDryrunV2 } from '@/app/avstemming/AvstemmingDryrunV2.tsx'
+import { AuditTestButton } from '@/app/avstemming/AuditTestButton.tsx'
 import { fetchAvstemminger } from '@/app/avstemming/actions.ts'
 import { checkToken } from '@/lib/server/auth'
 import { isSuccessResponse } from '@/lib/api/types'
@@ -25,6 +26,7 @@ export default async function AvstemmingPage() {
             <LatestAvstemminger xmlMessages={res.data} />
             <AvstemmingDryrun />
             <AvstemmingDryrunV2 />
+            <AuditTestButton />
         </div>
     )
 }
