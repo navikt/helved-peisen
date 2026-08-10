bfbae94f222cb9d1430727c96d6d7d93b2b5d6ab
Stephen Ramthun <stephenramthun@gmail.com>
2026-08-10 14:32:59 +0200

    #511 Trenger ikke å styre tidsrom for dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import { checkToken } from '@/lib/server/auth.ts'
 import { DashboardProvider } from '@/app/dashboard/DashboardContext.tsx'
-import { DashboardFiltere } from '@/app/dashboard/DashboardFiltere.tsx'
 import { DashboardContent } from '@/app/dashboard/DashboardContent.tsx'
 import { VStack } from '@navikt/ds-react'
 
@@ -10,10 +9,6 @@ export default async function DashboardPage() {
     return (
         <DashboardProvider>
             <VStack gap="space-32" className="w-full mx-auto p-4 ax-md:p-8">
-                <VStack gap="space-24">
-                    <DashboardFiltere />
-                </VStack>
-
                 <DashboardContent />
             </VStack>
         </DashboardProvider>
