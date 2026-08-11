4e8fe06a831ff90adce4b08c26f154a476bf1b7c
Stephen Ramthun <stephenramthun@gmail.com>
2026-08-11 09:33:55 +0200

    #511 Route til dashboard som default

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/kafka',
+                destination: '/dashboard',
                 permanent: false,
             },
         ]
