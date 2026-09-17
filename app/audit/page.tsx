d81db9a13f753b61ba4babe5bbc746990a9627b2
JorgenNyb <201098578+JorgenNyb@users.noreply.github.com>
2026-09-17 14:44:47 +0200

    #631 Lenk til gaal for andre auditl logs

--- app/audit/page.tsx
+++ app/audit/page.tsx
@@ -3,6 +3,7 @@ import { AuditFiltere, AuditSearchProvider } from '@/app/audit/AuditFiltere.tsx'
 import { AuditView } from '@/app/audit/AuditView.tsx'
 import { SortStateProvider } from '@/app/kafka/table/SortState'
 import { AuditMessagesProvider } from '@/app/audit/AuditMessagesContext.tsx'
+import { BodyShort, Link } from '@navikt/ds-react'
 
 import { checkToken } from '@/lib/server/auth.ts'
 
@@ -11,6 +12,17 @@ export default async function AuditOverview() {
 
     return (
         <section className="flex flex-col p-4">
+            <BodyShort className="mb-8">
+                Denne siden viser manuelle endringer gjort i peisen. For
+                (database)endringer fra audit-logger se{' '}
+                <Link
+                    href="https://audit-approval.iap.nav.cloud.nais.io/?team=helved"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Gjennomgang av audit-logg (Gaal)
+                </Link>
+            </BodyShort>
             <AuditFiltereProvider>
                 <AuditSearchProvider>
                     <AuditMessagesProvider>
